c4a2991e045030505b48c087b3f2a5083aec8da4
Max Calabro <maxccalabro@gmail.com>
2022-12-01 10:17:18 -0500

    created vars for required sections

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const siteInfo = {
+  //  who me?
   name: 'Max Clark Calabro',
-  title: 'Software Engineer'
+  title: 'Software Engineer',
+  whatIDo:
+    'I am a full stack engineer with a background in physics. Some other stuff blah blah.',
+
+  //  about me
+  aboutMe: 'This will be a big paragraph about me',
+
+  //  resume
+  resume: 'This will be the resume link',
+
+  //  socials / contact info
+  linkedIn: 'This will be the LinkedIn link',
+  gitHub: 'This will be the GitHub link'
 }
 
 const test = () => {
