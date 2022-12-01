5462f8f9e4b63942e4f0f081b768bd236ce43410
Max Calabro <maxccalabro@gmail.com>
2022-12-01 14:57:42 -0500

    clicked option will turn into a dot then change back when next option is clicked

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ const siteInfo = {
   //  about me
   aboutMe: 'This will be a big paragraph about me',
 
-  //  resume
+  //  resume / projects
   resume: 'This will be the resume link',
+  projects: 'This will be the projects link',
 
   //  socials / contact info
   linkedIn: 'This will be the LinkedIn link',
@@ -45,6 +46,10 @@ const project3 = {
 }
 
 //  buttons
+let aboutMe = document.createElement('div')
+aboutMe.className = 'button-aboutMe'
+aboutMe.innerHTML = 'About Me'
+
 let resume = document.createElement('div')
 resume.className = 'button-resume'
 resume.innerHTML = 'Resume'
@@ -56,6 +61,9 @@ projects.innerHTML = 'Projects'
 let contactInfo = document.createElement('div')
 contactInfo.className = 'button-contactInfo'
 contactInfo.innerHTML = 'Contact Info'
+
+//  track which element is a dot
+let trackDot = 0
 
 //
 //  functions
@@ -92,45 +100,98 @@ const fillHeader = () => {
   document.querySelector('header').append(headerInfo)
   document.querySelector('.name').innerHTML =
     siteInfo.name + '<br></br>' + siteInfo.title + '<br></br>'
+  document.querySelector('header').append(aboutMe)
   document.querySelector('header').append(resume)
   document.querySelector('header').append(projects)
   document.querySelector('header').append(contactInfo)
 }
 
 const mainDisplay = () => {
-  document.querySelector('.main-display').style.height = '100px'
-  document.querySelector('.main-display').style.width = '100px'
+  document.querySelector('.main-display').style.height = '200px'
+  document.querySelector('.main-display').style.width = '200px'
   let section = document.createElement('section')
   document.querySelector('.main-display').append(section)
 }
 
 const fillMainDisplay = (show) => {
+  //  remove current text from section
+  //document.querySelector('div').remove()
+  let mainView = document.createElement('div')
+  mainView.className = 'main-view'
+  document.querySelector('section').append(mainView)
   switch (show) {
+    case 'aboutMe':
+      trackDot = 0
+      fillMainView(siteInfo.aboutMe, aboutMe)
+      break
     case 'resume':
-      console.log(show)
-      //  call a function called showResume
+      trackDot = 1
+      fillMainView(siteInfo.resume, resume)
       break
     case 'projects':
-      console.log(show)
-      //  call a function called showprojects
+      trackDot = 2
+      fillMainView(siteInfo.projects, projects)
       break
     case 'contactInfo':
-      console.log(show)
-      //  call a function called showcontactInfo
+      trackDot = 3
+      fillMainView(siteInfo.email, contactInfo)
       break
     default:
-      console.log('default')
-      //  call a function called showAboutMe
+      trackDot = 0
+      fillMainView(siteInfo.aboutMe, aboutMe)
       break
   }
 }
 
+const fillMainView = (siteInfoKey, changeToDot) => {
+  changeToDot.innerHTML = '.'
+  document.querySelector('.main-view').innerHTML = siteInfoKey
+  undoDot()
+}
+
+const undoDot = () => {
+  switch (trackDot) {
+    case 0:
+      ChangeAllButOne(0)
+      break
+    case 1:
+      ChangeAllButOne(1)
+      break
+    case 2:
+      ChangeAllButOne(2)
+      break
+    case 3:
+      ChangeAllButOne(3)
+      break
+    default:
+      break
+  }
+}
+
+const ChangeAllButOne = (dotValue) => {
+  if (dotValue != 0) {
+    aboutMe.innerHTML = 'About Me'
+  }
+  if (dotValue != 1) {
+    resume.innerHTML = 'Resume'
+  }
+  if (dotValue != 2) {
+    projects.innerHTML = 'Projects'
+  }
+  if (dotValue != 3) {
+    contactInfo.innerHTML = 'Contact Info'
+  }
+}
 //
 //  listeners
 //
 
 //  event listener that sends info to fillMainDisplay
 const createListeners = () => {
+  document.querySelector('.button-aboutMe').addEventListener('click', () => {
+    fillMainDisplay('aboutMe')
+  })
+
   document.querySelector('.button-resume').addEventListener('click', () => {
     fillMainDisplay('resume')
   })
